7012e60e6898ae56bf9c6ed8fe9b5fd04db41be8
AaronToh <aarontywei@gmail.com>
2025-08-19 01:32:49 +0800

    Renamed, Greets, Exits

---
+++ src/main/java/Ming.java
@@ -0,0 +1,5 @@
+public class Ming {
+    public static void main(String[] args) {
+        System.out.println("Hello from! I'm Ming\n" + "What can I do for you?\n" + "\nBye. Hope to see you again!");
+    }
+}
