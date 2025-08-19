98445062c60b9f4a70e10ac35930006efe5cdd7c
AaronToh <aarontywei@gmail.com>
2025-08-19 10:15:24 +0800

    Marks Tasks

--- src/main/java/Ming.java
+++ src/main/java/Ming.java
@@ -5,23 +5,40 @@ import java.util.Scanner;
 public class Ming {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> list = new ArrayList<>(100);
+        List<Task> list = new ArrayList<>(100);
 
         System.out.println("Hello from! I'm Ming\n" + "What can I do for you?\n");
+
         while (true) {
             String input = scanner.nextLine();
-            if (input.equals("list")) {
+
+            Scanner lineScanner = new Scanner(input);
+            String command = lineScanner.next();
+
+            if (command.equals("mark")) {
+                int id = lineScanner.nextInt();
+                Task task = list.get(id - 1);
+                task.markAsDone();
+                System.out.println("I've marked this task as dene:\n" + task.toString());
+            }  else if (command.equals("unmark")) {
+                int id = lineScanner.nextInt();
+                Task task = list.get(id - 1);
+                task.markAsNotDone();
+                System.out.println("I've unmarked this task as dene:\n" + task.toString());
+            } else if (input.equals("list")) {
                 int i = 1;
-                for (String item : list) {
-                    System.out.println(i + ". " + item);
+                System.out.println("Here are the tasks in your list:");
+                for (Task task : list) {
+                    System.out.println(task.toString());
                     i++;
                 }
             } else if (input.equals("bye")){
                 System.out.println("Goodbye!");
                 return;
             } else {
-                list.add(input);
-                System.out.println("added: " + input);
+                Task task = new Task(input);
+                list.add(task);
+                System.out.println("added: " + task.getDescription());
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return (isDone ? "[X] " : "[ ] ") + description;
+    }
+}
